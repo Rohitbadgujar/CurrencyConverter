f35c0187cd6a67ce03ec277464873cab380eb4db
Rohit <tyson@rohits-mac.local>
2019-04-13 18:24:12 -0700

    Simple currency converter application

--- MoneyConverter/ViewController.swift
+++ MoneyConverter/ViewController.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets and Action
+    
+    @IBOutlet weak var euroTextField: UITextField!
+    @IBOutlet weak var dollarTextField: UITextField!
+    
+    @IBAction func clearAction(_ sender: UIButton) {
+        // clear textView
+        euroTextField.text = ""
+        dollarTextField.text = ""
+        
+    }
+    
+    @IBAction func convertAction(_ sender: UIButton) {
+        
+        if euroTextField.text != "" && dollarTextField.text == "" {
+            
+            //read and convert from euro
+            let euroAmount = Double(euroTextField.text!)
+            //convert
+            let dollarAmount = directConvert(amount: euroAmount!, rate: 1.234)
+            
+            dollarTextField.text = "\(dollarAmount)"
+        }
+        else if euroTextField.text == "" && dollarTextField.text != "" {
+            
+            //read and convert from euro
+            let dollarAmount = Double(dollarTextField.text!)
+            //convert
+            var euroAmount = invertConvert(amount: dollarAmount!, rate: 1.234)
+            euroAmount = (euroAmount*100).rounded()/100
+            euroTextField.text = "\(euroAmount)"
+        }
+        else {
+            
+            print("Not possible")
+        }
+    }
+    func directConvert(amount: Double, rate: Double) -> Double {
+        return amount * rate
+    }
+    func invertConvert(amount: Double, rate: Double) -> Double {
+        return amount / rate
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
